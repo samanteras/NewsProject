c7238767aa697ccc36b2f42ff60966f09dc18ba3
samanteras <samanter@mail.ru>
2023-02-01 21:58:24 +0300

    Create model

---
+++ NewsProject/Model/Article.swift
@@ -0,0 +1,19 @@
+//
+//  Article.swift
+//  NewsProject
+//
+//  Created by MAC13 on 01.02.2023.
+//
+
+import Foundation
+
+struct Article {
+    let sourceName: String
+    let author: String
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String
+    let publishedAt: String
+    
+}
---
+++ NewsProject/Model/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  NewsProject
+//
+//  Created by MAC13 on 01.02.2023.
+//
+
+import Foundation
+
+let articles: [Article] = []
+
+private func loadNews() {
+    
+}
